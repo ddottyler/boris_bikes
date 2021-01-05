ee6d7d450266ab4f3660b4789ab5463f0657e1d3
Dan T <dp.tyler@hotmail.co.uk>
2021-01-05 16:28:01 +0000

    Step 6 complete

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,9 @@
+require 'docking_station'
+
+describe DockingStation do
+    
+    # it 'returns "fizzbuzz" when number % 3 == 0 && number % 5 == 0' do
+    #     expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    #   end
+
+end
